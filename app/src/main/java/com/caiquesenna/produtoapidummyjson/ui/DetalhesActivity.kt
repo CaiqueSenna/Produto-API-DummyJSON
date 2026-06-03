08a0b6f770c5f664362f5d36808373184a50a2fa
CaiqueSenna <caiquesenna@hotmail.com>
2026-06-03 19:26:08 -0300

    feat: Criado Comunicação Binding com DetalhesActivity

--- app/src/main/java/com/caiquesenna/produtoapidummyjson/ui/DetalhesActivity.kt
+++ app/src/main/java/com/caiquesenna/produtoapidummyjson/ui/DetalhesActivity.kt
@@ -3,19 +3,26 @@ package com.caiquesenna.produtoapidummyjson.ui
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.caiquesenna.produtoapidummyjson.R
+import com.bumptech.glide.Glide
+import com.caiquesenna.produtoapidummyjson.data.Product
+import com.caiquesenna.produtoapidummyjson.databinding.ActivityDetalhesBinding
 
 class DetalhesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetalhesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detalhes)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        val product = intent.getSerializableExtra("PRODUCT") as? Product
+        product?.let {
+            binding.txtDetailTitle.text = it.title
+            binding.txtDetailPrice.text = "R$ ${it.price}"
+            binding.txtDetailDescription.text = it.description
+            Glide.with(this).load(it.thumbnail).into(binding.imgDetailProduct)
         }
+
     }
 }
