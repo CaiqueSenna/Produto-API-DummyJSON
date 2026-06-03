13a84407c1f9030f71adf00112889d29fe82c800
CaiqueSenna <caiquesenna@hotmail.com>
2026-06-03 19:10:24 -0300

    feat: Criado Activity Detalhes

---
+++ app/src/main/java/com/caiquesenna/produtoapidummyjson/ui/DetalhesActivity.kt
@@ -0,0 +1,21 @@
+package com.caiquesenna.produtoapidummyjson.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.caiquesenna.produtoapidummyjson.R
+
+class DetalhesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalhes)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
